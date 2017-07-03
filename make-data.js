89fbca45af7271227967c223df943284ffc417a6
Tucker <takahirosuzuki.m010@gmail.com>
2017-07-03 20:40:32 +1000

    initial commit

---
+++ make-data.js
@@ -0,0 +1,36 @@
+//generate height, weight, BMI data
+const DATA_SIZE = 50000
+const data = []
+
+//generate the data
+for (var i = 0; i < DATA_SIZE; i++) {
+    let height = rand(120, 200);  //training/test?
+    let weight = rand(30, 100);
+    let bmi = calc_bmi(height, weight);
+    let f = {
+        'input': [height, weight],
+        'putput': bmi ?[0, 1] : [1, 0]
+    };
+    data.push(f);
+ }
+
+//store the data into the file
+data_json = JSON.stringify(data);
+const fs = require('fs');
+fs.writeFileSync("bmi.json", data_json);
+
+console.log("Successfully, the data saved!!");
+
+
+//calculate BMI and judge whether obesity or not
+function calc_bmi(height, weight) {
+    let m = height / 100;
+    let v = weight / (m * m);
+    return (v > 25);
+ }
+
+//generate arbitrary scope of random number
+function rand(n, m) {
+    let range = m - n + 1;
+    return Math.floor(Math.random() * range + n)
+ }
